089e72ea0250ff6e9b96a872f0f078218385bddc
xdicecle <myersmjustin@yahoo.com>
2026-01-12 15:02:55 -0600

    Added some styling to the calculator display and fixed spacing issues in the JavaScript code.

--- js/script.js
+++ js/script.js
@@ -31,15 +31,12 @@ let ans = 0;
 let op = false;
 let check = false;
 let numberKeyPressed = false;
+let firstKeyPressed = true;
 let equalKeyPressed =false;
 
 //operater function
 
 function operater(sign){
-
-    num1 = String(num1).replaceAll(" ","");
-    num2 = String(num2).replaceAll(" ","");
-
     num1 = +num1;
     num2 = +num2;
     if(sign == "+") ans = add(num1,num2);
@@ -65,6 +62,10 @@ let display = document.querySelector("#nums")
     if(e.target.tagName !== "BUTTON") return;
     else{
         let btn = e.target.id;
+        if(firstKeyPressed && display.textContent == "0"){
+            firstKeyPressed = false;
+            display.textContent = "";
+        } 
         if(equalKeyPressed){
             display.textContent = "";
             num1 = 0;
@@ -82,13 +83,15 @@ let display = document.querySelector("#nums")
             string = 0;
             sign = 0;
             op = 0
+            display.textContent = "0";
+            firstKeyPressed = true;
         }
         else if(check){
             check = false;
             numberKeyPressed = true;
             display.textContent = "";
              if(display.textContent !=" +" && display.textContent !=" -" && display.textContent !=" *" 
-            && display.textContent !=" /" ) display.textContent += ` ${btn}`;
+            && display.textContent !=" /" ) display.textContent += `${btn}`;
         }
         else if(op && (btn == "+" || btn == "*" || btn == "-" || btn == "/")){
 
@@ -121,7 +124,7 @@ let display = document.querySelector("#nums")
         else if(btn != '='){
             numberKeyPressed = true;
              if(display.textContent !=" +" && display.textContent !=" -" && display.textContent !=" *" 
-            && display.textContent !=" /" )display.textContent += ` ${btn}`;
+            && display.textContent !=" /" )display.textContent += `${btn}`;
         }
 
         
